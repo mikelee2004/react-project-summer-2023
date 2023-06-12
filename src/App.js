ae34f8b75daee25b0fd0cb6b18e6c788aabca7ff
mikelee2004 <strober2004@gmail.com>
2023-06-12 17:22:29 +0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import Header from "./components/Header/header";
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+      <main style={{ background: 'blue', height: '2000px' }}></main>
+      <footer style={{ background: 'red', position: 'fixed', bottom: '0', width: '100%', height: '200px' }}>Footer</footer>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
